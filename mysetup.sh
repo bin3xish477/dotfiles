dd1605e9c1a8d64c4537a7fb38b2819c9154fc8c
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-01-16 11:56:27 -0500

    Create mysetup.sh

---
+++ mysetup.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+main() {
+  install_vimplug
+  echo -e "==> installed vim plug..."
+
+  curl -s -o "$HOME/.vimrc" "https://raw.githubusercontent.com/bin3xish477/dotfiles/master/.vimrc"
+  echo -e "==> downloaded vimrc file..."
+
+  curl -s -o "$HOME/.tmux.conf" "https://raw.githubusercontent.com/bin3xish477/dotfiles/master/.tmux.conf"
+  echo -e "==> downloaded tmux config file..."
+
+  if [[ "$SHELL" =~ "zsh" ]]; then
+    sed -i 's/bash/zsh/g' "$HOME/.tmux.conf"
+  fi
+}
+
+install_vimplug() {
+  curl -s -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+}
+
+main
+
+echo "==> setup complete..."
