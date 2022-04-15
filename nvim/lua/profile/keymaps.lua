8479b49ccd4ab76dd8aa8fed20bd9b857acc4edc
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-04-15 16:42:57 -0400

    Update keymaps.lua

--- nvim/lua/profile/keymaps.lua
+++ nvim/lua/profile/keymaps.lua
@@ -14,5 +14,22 @@ local opts = {
 
 local keymap = vim.api.nvim_set_keymap
 
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
 -- remap `jj` to ESC in insert mode
 keymap("i", "jj", "<esc>", opts)
+
+-- movement key remappings ------------------
+
+-- move between windows quicker
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- resize windows with arrow keys
+keymap("n", "<C-UP>", ":resize -2<cr>", opts)
+keymap("n", "<C-Down>", ":resize +2<cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<cr>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<cr>", opts)
