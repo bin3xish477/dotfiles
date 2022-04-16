2e0485b46a4785cc110c2aa8b979455dde792018
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-04-16 16:58:31 -0400

    Update keymaps.lua

--- nvim/lua/profile/keymaps.lua
+++ nvim/lua/profile/keymaps.lua
@@ -5,6 +5,7 @@ Keymapping Modes:
   - Visual Mode = "v"
   - Visual Block Mode = "x"
   - Command Mode = "c"
+  - Terminal Mode = "t"
 --]]
 
 local opts = {
@@ -20,8 +21,6 @@ vim.g.maplocalleader = ","
 -- remap `jj` to ESC in insert mode
 keymap("i", "jj", "<esc>", opts)
 
--- movement key remappings ------------------
-
 -- move between windows quicker
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -29,7 +28,30 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- resize windows with arrow keys
-keymap("n", "<C-UP>", ":resize -2<cr>", opts)
-keymap("n", "<C-Down>", ":resize +2<cr>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<cr>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<cr>", opts)
+keymap("n", "<C-UP>", ":resize +2<cr>", opts)
+keymap("n", "<C-Down>", ":resize -2<cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize +2<cr>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<cr>", opts)
+
+keymap("n", "<leader>db", ":normal gg<s-v>Gd<cr>", opts)
+keymap("n", "<leader>dfl", "<s-v>ggd<cr>", opts)
+keymap("n", "<leader>", "<s-v>Gd<cr>", opts)
+
+-- FZF
+keymap("n", "<leader>ff", ":Files<cr>", opts)
+
+-- NvimTree
+keymap("n", "<leader>t", ":NvimTreeToggle<cr>", opts)
+keymap("n", "<leader>r", ":NvimTreeRefresh<cr>", opts)
+
+-- Barbar
+keymap("n", "<leader>bp", ":BufferPrevious<cr>", opts)
+keymap("n", "<leader>bn", ":BufferNext<cr>", opts)
+keymap("n", "<leader>bc", ":BufferClose<cr>", opts)
+
+-- exit terminal with ESC
+keymap("t", "<esc>", "<c-\\><c-n>:q!<esc>", opts)
+
+keymap("n", "ev", ":split $MYVIMRC<cr>", opts)
+keymap("n", "sv", ":w<cr> :source $MYVIMRC<cr> :q<cr>", opts)
+-- keymap("", "", "", opts)
