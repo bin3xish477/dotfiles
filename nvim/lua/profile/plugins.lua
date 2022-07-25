aba1494fc52bd8c07eaaea8c6507f70a6712335a
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-07-24 20:16:25 -0400

    Update plugins.lua

--- nvim/lua/profile/plugins.lua
+++ nvim/lua/profile/plugins.lua
@@ -51,7 +51,7 @@ packer.startup(function()
         'kyazdani42/nvim-web-devicons',
       },
       config = function() require('nvim-tree').setup {
-        open_on_setup = true,
+        open_on_setup = false,
         open_on_tab = false,
         view = {
           width = 30,
