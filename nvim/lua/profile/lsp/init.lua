29d8cc08738ab98c64f4e833d2b3dbf0712acee6
Alexis Rodriguez <rodriguez10011999@gmail.com>
2022-08-11 15:44:35 -0400

    adding

--- nvim/lua/profile/lsp/init.lua
+++ nvim/lua/profile/lsp/init.lua
@@ -1,6 +1,20 @@
-local ok, _ = pcall(require, "lspconfig")
+local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then
   return
 end
 
 require 'profile.lsp.mason'
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local servers = {
+  'gopls', 'rust_analyzer', 'pyright', 'marksman',
+  'taplo', 'bashls', 'sumneko_lua',
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    capabilities = capabilities,
+  }
+end
