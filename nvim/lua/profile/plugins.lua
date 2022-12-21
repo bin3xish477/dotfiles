3f8a52928233742d6a5db985954cf1205b84b537
Alexis Rodriguez <arodriguez99@protonmail.com>
2022-12-20 19:00:24 -0500

    adding automatic packer.nvim installation script Lua code block from tjdevries kickstart.nvim init file

--- nvim/lua/profile/plugins.lua
+++ nvim/lua/profile/plugins.lua
@@ -1,3 +1,12 @@
+-- Install packer, credits to @tjdevries: https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  vim.cmd [[packadd packer.nvim]]
+end
+
 local packer = require('packer')
 
 packer.init {
@@ -230,4 +239,7 @@ packer.startup(function(use)
   use 'folke/tokyonight.nvim'
   -- use ''
 
+  if is_bootstrap then
+    require('packer').sync()
+  end
 end)
